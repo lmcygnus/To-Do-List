4bf1c68109c1ec146d36e3e971097c283ddf685e
lmcygnus <mloida591@gmail.com>
2024-02-16 17:55:44 -0300

    display grid

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './style.css';
-import { projects, ToDo, createNewProject, createNewToDo } from './toDo';}
+// import { projects, ToDo, createNewProject, createNewToDo } from './toDo';
 
 const body = document.querySelector("body");
 
+const createDisplayDivision = (parent) => {
+    const sidebar = document.createElement("div");
+    const mainContent = document.createElement("div");
+
+    sidebar.classList.add("sidebar");
+    mainContent.classList.add("mainContent");
+
+    sidebar.textContent = "Contenido de la barra lateral";
+    mainContent.textContent = "Contenido principal";
+
+    parent.appendChild(sidebar);
+    parent.appendChild(mainContent);
+}
+
+createDisplayDivision(body);
