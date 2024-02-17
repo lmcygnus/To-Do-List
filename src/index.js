ec1900ebf302521c9ff3ec09c0ebe10c4fa0846d
lmcygnus <mloida591@gmail.com>
2024-02-17 13:45:13 -0300

    create Dialog

--- src/index.js
+++ src/index.js
@@ -2,36 +2,9 @@ import "./style.css";
 // import { projects, ToDo, createNewProject, createNewToDo } from './toDo';
 
 const body = document.querySelector("body");
+const dialog = document.querySelector("dialog");
+const newTodo = document.querySelector("#newToDo");
 
-const createDisplayDivision = (parent) => {
-  const sidebar = document.createElement("div");
-  const mainContent = document.createElement("div");
-
-  sidebar.classList.add("sidebar");
-  mainContent.classList.add("mainContent");
-
-  sidebar.textContent = "Contenido de la barra lateral";
-  mainContent.textContent = "Contenido principal";
-
-  parent.appendChild(sidebar);
-  parent.appendChild(mainContent);
-};
-
-createDisplayDivision(body);
-
-const createSidebarContent = (parent) => {
-  const title = document.createElement("h1");
-  title.textContent = "To Do List";
-  parent.appendChild(title);
-
-  const today = document.createElement("h4");
-  today.textContent = "Today";
-  parent.appendChild(today);
-
-  const projects = document.createElement("h3");
-  projects.textContent = "Projects";
-  parent.appendChild(projects);
-};
-
-const sidebar = document.querySelector(".sidebar");
-createSidebarContent(sidebar);
+newTodo.onclick = () => {
+  dialog.show();
+}
